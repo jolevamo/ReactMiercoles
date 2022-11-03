1ee0d86da2ef45ca9eb409841408575a20dd8bd8
Orma Development <orion_alzate@hotmail.com>
2022-11-02 20:34:32 -0500

    Proyecto React

---
+++ src/Integrantes/Integrantes.js
@@ -0,0 +1,9 @@
+export function Integrantes (){
+
+    return(
+        <>
+        <h1>Integrantes trabajando</h1>
+        </>
+
+    )
+}
---
+++ src/Routes/Routes.js
@@ -0,0 +1,24 @@
+import {Home} from '../Home/Home.js'
+import {Historia} from '../Historia/Historia.js'
+import {Integrantes} from '../Integrantes/Integrantes.js'
+import {Menu} from '../Menu/Menu.js'
+import {Music} from '../Music/Music.js'
+
+
+import { Routes, Route } from "react-router-dom";
+
+
+
+export function Rutas(){
+    return(
+            <div className="App">
+                <Menu/>
+                <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="historia" element={<Historia />} />
+                <Route path="integrantes" element={<Integrantes />} />
+                <Route path="music" element={<Music />} />
+                </Routes>
+            </div>
+         );
+}
